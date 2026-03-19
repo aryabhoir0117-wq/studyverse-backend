5baa6c6d2835c98537d3b6a46f84d2883fe11848
aryabhoir0117-wq <aryabhoir0117@gmail.com>
2026-03-20 00:03:08 +0530

    next

--- models/User.js
+++ models/User.js
@@ -1,42 +1,3 @@
-// const mongoose = require("mongoose");
-// const bcrypt = require("bcryptjs");
-
-// const userSchema = new mongoose.Schema({
-//   username: {
-//     type: String,
-//     required: true
-//   },
-//   email: {
-//     type: String,
-//     required: true,
-//     unique: true
-//   },
-//   password: {
-//     type: String,
-//     required: true
-//   },
-//   xp: {
-//     type: Number,
-//     default: 0
-//   },
-//   role: {
-//     type: String,
-//     enum: ["user", "admin"],
-//     default: "user"
-//   }
-// }, { timestamps: true });
-
-// userSchema.pre("save", async function(next) {
-//   if (!this.isModified("password")) return next();
-//   this.password = await bcrypt.hash(this.password, 10);
-//   next();
-// });
-
-// userSchema.methods.matchPassword = async function(enteredPassword) {
-//   return await bcrypt.compare(enteredPassword, this.password);
-// };
-
-// module.exports = mongoose.model("User", userSchema);
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
@@ -84,9 +45,9 @@ const userSchema = new mongoose.Schema({
     default: "Cabin Boy"
   },
   lastTreasureClaim: {
-  type: String,
-  default: ""
-}
+    type: String,
+    default: ""
+  }
 }, { timestamps: true });
 
 userSchema.pre("save", async function () {
