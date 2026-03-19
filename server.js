49d9da796f76a98e258a25300b0466b25b3f5b0c
aryabhoir0117-wq <aryabhoir0117@gmail.com>
2026-03-19 23:00:33 +0530

    fix:server

--- server.js
+++ server.js
@@ -18,11 +18,15 @@ app.get("/", (req, res) => {
 });
 // Routes
 app.use("/api/auth", authRoutes);
-app.use("/api/user", userRoutes);
 
 // Server
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
